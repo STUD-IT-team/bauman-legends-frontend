c8b20a76976c59a7cbc81e7c04d4b26e9dc2852a
SodAcetate <sodium.acetate.82@gmail.com>
2023-10-06 18:53:59 +0300

    Страница профиля

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -6,15 +6,36 @@
 .root-profile
   width 100%
   height 100%
+  
   .navbar
     width 100%
-    margin 0
+    margin-bottom 20px
     background-color colorBg
     padding 10px
     font-large()
     color colorText1
     align-content: space-between
     text-align left
+  .header
+      font-large()
+      font-bold()
+      margin 10px 20px
+      color colorBg
+  .tasks
+    padding 0
+    margin 0 20px
+    display grid
+    grid-template-columns 1fr 1fr
+    grid-auto-rows: 1fr;
+    column-gap 5px
+    row-gap 5px
+    .task-button
+      button()
+      border-radius 8px
+      background colorBg
+      color colorText1      
+      width 100%
+      height 100%
   .box
     background-color colorBg
     border-radius borderRadiusM
@@ -22,14 +43,14 @@
     padding-left 20px
     padding-right 20px
     padding-bottom 20px
-    margin 20px 20px
+    margin 10px 20px
     text-align left
     align-content left
     font-medium()
-    font-bold()
     color colorText1
     .s1
       font-large()
+      font-bold()
       margin auto
       display inline-block
     .s2
@@ -44,34 +65,61 @@
       margin 0
       padding 0
       display inline-block
-    .task-button
-      button()
-      width auto
-      margin-top 10px
-      margin-left 0px
-      margin-right 10px
-      padding 5px 10px
-      display inline-block
+    .name-row
+      display flex
+      margin-top 5px
+      .name
+        flex 1
+        margin auto
+    .data-row
+      display flex
+      margin 10px 0
+      .field
+        color colorBgLightExtra
+      .data
+        flex 1
+        margin-left 10px
+        color colorText1
+      .edit
+        button()
+        width auto
+        margin 0
+        padding 0
+        background colorBg
+        font-small-extra()
+        color colorBgLightExtra
     .dropdown
       width auto
-      background colorButton
+      background colorBg
+      text-small()
+      color colorText1
       display inline-block
       padding 2px 10px
-      margin-top 5px
-      margin-left 10px
-      border solid 0px
-      border-radius 1000px
+      margin 0
+      border colorBgLightExtra solid 1px
+      border-radius 8px
 
 </style>
 
 <template>
   <div class="root-profile">
+    <img src="../res/images/Gerbs.png" class="logo">
     <div>
     <table class="navbar">
     <th>Профиль/Главная</th>
     <th>00:04:20</th>
     </table>
     </div>
+    <div class="header">ДОСТУПНЫЕ ЗАДАНИЯ</div>
+    <div class="tasks">
+      <router-link style="text-decoration: none" :to="{name: 'login'}">
+      <button class="task-button">Онлайн-задание</button>
+      </router-link>
+      <button class="task-button">Фото-задание</button>
+      <button class="task-button">Путешествие в НОЦ</button>
+      <button class="task-button">Задание с актёром</button>
+    </div>
+    <div class="header">МОЯ КОМАНДА</div>
     <div class="box">
       <p class="s1">Умные мИУсли</p>
       <p class="s2">#0002</p><br>
@@ -79,22 +127,65 @@
       <br><br>
       <!-- Это бы в отдельную вьюшку -->
       <p class="s1">Состав команды:</p><br>
-      Луцук Ренат
-      <p class="s3">Капитан</p>
-      <br>
-      Мельников Дмитрий
-      <br>
-      Новиков Тимофей
-      <br>
-      <br>
-      <p class="s1">Задачи</p><br>
-      <router-link class="signin-link" :to="{name: 'login'}">
-        <button class="task-button">Задача 1</button>
-        <button class="task-button">Задача 2</button>
-        <button class="task-button">Задача 3</button>
-      </router-link>
+      <div class="name-row">
+      <div class="name">Луцук Ренат</div>
+      <select class=dropdown>
+        <option selected hidden>Роль</option>
+        <option>Капитан</option>
+        <option>Зам</option>
+        <option>Участник</option>
+      </select>
+      </div>
+      <div class="name-row">
+      <div class="name">Мельников Дмитрий</div>
+      <select class=dropdown>
+        <option selected hidden>Роль</option>
+        <option>Капитан</option>
+        <option>Зам</option>
+        <option>Участник</option>
+      </select>
+      </div>
+      <div class="name-row">
+      <div class="name">Новиков Тимофей</div>
+      <select class=dropdown>
+        <option selected hidden>Роль</option>
+        <option>Капитан</option>
+        <option>Зам</option>
+        <option>Участник</option>
+      </select>
+      </div>
     </div>
-    <img src="../res/images/Gerbs.png" class="logo">
+    <div class="header">ПРОФИЛЬ</div>
+    <div class="box">
+    <div class="s1">ФАМИЛИЯ ИМЯ ОТЧЕСТВО</div>
+    <div class="data-row">
+      <div class="field">Группа:</div>
+      <div class="data">ИУ4-72б</div>
+      <button class="edit">Изменить</button>
+    </div>
+    <div class="data-row">
+      <div class="field">Email:</div>
+      <div class="data">my_mail@mail.com</div>
+      <button class="edit">Изменить</button>
+    </div>
+    <div class="data-row">
+      <div class="field">Telegram:</div>
+      <div class="data">@OneVVTG</div>
+      <button class="edit">Изменить</button>
+    </div>
+    <div class="data-row">
+      <div class="field">Вконтакте:</div>
+      <div class="data">vk.com/onevvvk</div>
+      <button class="edit">Изменить</button>
+    </div>
+    <div class="data-row">
+      <div class="field">Номер телефона:</div>
+      <div class="data">8-800-555-35-35</div>
+      <button class="edit">Изменить</button>
+    </div>
+
+
+    </div>
     <!-- <div class="title"> PROFILE PAGE</div>
     <div class="name">Rudy</div>
     <div class="email">r@mail.ru</div>
