28fd97f1588c041fd310882a72b654632ce39198
Сергей Тяпкин <Tyapkin2002@mail.ru>
2023-10-10 02:32:52 +0300

    Вернул закрытый доступ к профилю

--- src/Router.js
+++ src/Router.js
@@ -12,7 +12,7 @@ export default function createVueRouter(Store) {
   const routes = [
     {path: '/register', name: 'register', component: Registration, meta: {noLoginRequired: true}},
     {path: '/login', name: 'login', component: SignIn, meta: {noLoginRequired: true}},
-    {path: '/profile', name: 'profile', component: Profile, meta: {loginRequired: false}},
+    {path: '/profile', name: 'profile', component: Profile, meta: {loginRequired: true}},
     {path: '/login/email', name: 'signInByEmail', component: SignIn, meta: {noLoginRequired: true}},
     {path: '/password/change', name: 'changePassword', component: ChangePassword, meta: {loginRequired: true}},
     {path: '/password/restore', name: 'restorePassword', component: SignIn, meta: {noLoginRequired: true}},
